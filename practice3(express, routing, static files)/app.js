c5755ceae91ce82789149d05e23c6728c61e22bf
Ariel Nykanen <arielnykan3n@gmail.com>
2019-01-09 03:23:35 +0200

    finished-practice-3

---
+++ practice3(express, routing, static files)/app.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const rootDir = require('./util/path')
+const express = require('express');
+  // routing exports dont need to be in order unless you use "use"
+const userRouting = require('./routes/user');
+const homeRouting = require('./routes/home');
+app = express();
+app.listen(3000);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/user', userRouting);
+app.use(homeRouting);
+app.use((req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', '404.html'))
+});
+ 
