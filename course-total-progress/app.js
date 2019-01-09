07543a6fc384d201ecd47d3702979ea8b3a5699e
Ariel Nykanen <arielnykan3n@gmail.com>
2019-01-09 03:33:26 +0200

    small change

--- course-total-progress/app.js
+++ course-total-progress/app.js
@@ -1,5 +1,17 @@
-const http = require('http');
-const routes = require('./routes');
-const server = http.createServer(routes);
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoute);
+app.use(shopRoute);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+app.listen(3000);
