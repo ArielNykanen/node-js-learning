a0a9c767d833254003fb911680b46ce312193770
Ariel Nykanen <arielnykan3n@gmail.com>
2019-01-14 07:54:56 +0200

    adding sequelize setup in app.js + adding dummy user+ cart

--- course-total-progress/app.js
+++ course-total-progress/app.js
@@ -5,6 +5,11 @@ const app = express();
 
 const errorCtrl = require('./controllers/errors')
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -14,15 +19,48 @@ const shopRoute = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+User.findByPk(1)
+.then(user => {
+  req.user = user;
+  next();
+})
+.catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoute);
 app.use(shopRoute);
 
 app.use(errorCtrl.get404);
 
+// relations
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem});
+Product.belongsToMany(Cart, { through: CartItem});
+// use it when you want to overite db
+// sequelize.sync({force: true}).then(
 sequelize.sync().then(
   (result) => {
+    return User.findByPk(1);
     // console.log(result);
   }
 )
-.catch(err => console.log(err));
-app.listen(3000);
+.then((user) => {
+  if (!user) {
+    return User.create({name: 'user1', email: 'test@gmail.com'});
+  }
+  // return Promise.resolve(user);
+    // no need to declare in here. it will be automaticly promise type
+  return user;
+})
+.then(user => {
+  // console.log(user);
+  return user.createCart();
+}).then(cart => {
+  console.log("Cart was created" + cart);
+  app.listen(3000);
+})
+.catch(err => console.log(err)); 
