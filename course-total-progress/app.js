25ee84092f1b81d479e51637a1669a39b4665dab
Ariel Nykanen <arielnykan3n@gmail.com>
2019-01-14 11:38:00 +0200

    removing sequelize and adding mongodb connection

--- course-total-progress/app.js
+++ course-total-progress/app.js
@@ -4,14 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const errorCtrl = require('./controllers/errors')
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
-
+const mongoConnect = require('./util/database').mongoConnect;
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -22,12 +15,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-User.findByPk(1)
-.then(user => {
-  req.user = user;
-  next();
-})
-.catch(err => console.log(err));
+// User.findByPk(1)
+// .then(user => {
+//   req.user = user;
+//   next();
+// })
+// .catch(err => console.log(err));
+next();
 });
 
 app.use('/admin', adminRoute);
@@ -35,37 +29,7 @@ app.use(shopRoute);
 
 app.use(errorCtrl.get404);
 
-// relations
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem});
-Product.belongsToMany(Cart, { through: CartItem});
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem })
-// use it when you want to overite db
-// sequelize.sync({force: true}).then(
-sequelize.sync().then(
-  (result) => {
-    return User.findByPk(1);
-    // console.log(result);
-  }
-)
-.then((user) => {
-  if (!user) {
-    return User.create({name: 'user1', email: 'test@gmail.com'});
-  }
-  // return Promise.resolve(user);
-    // no need to declare in here. it will be automaticly promise type
-  return user;
-})
-.then(user => {
-  // console.log(user);
-  return user.createCart();
-}).then(cart => {
-  console.log("Cart was created" + cart);
+mongoConnect(() => {
   app.listen(3000);
 })
-.catch(err => console.log(err)); 
+ 
