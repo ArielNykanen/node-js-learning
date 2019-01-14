a034a3dd52ba2caf786b9e165823065c5cbf9ef1
Ariel Nykanen <arielnykan3n@gmail.com>
2019-01-14 11:36:06 +0200

    changing database to mongodb

--- course-total-progress/util/database.js
+++ course-total-progress/util/database.js
@@ -1,9 +1,27 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-  'node_shop', 
-  'root', 
-  '121212', 
-  {dialect: 'mysql', host: 'localhost'}
-);
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://ariel:12131415@cluster0-4a0ak.mongodb.net/test?retryWrites=true')
+  .then(client => {
+    _db = client.db();
+    console.log('Connected!');
+    callback();
+  })
+  .catch(err => {
+    console.log(err)
+    throw err;
+  });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
