b8a93e2958d44e60df498a95934228ecf93bef6a
Ariel Nykanen <arielnykan3n@gmail.com>
2019-01-15 06:59:45 +0200

    adding temp user to work with

--- course-total-progress/app.js
+++ course-total-progress/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 const errorCtrl = require('./controllers/errors')
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user'); 
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -13,15 +14,14 @@ const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 app.use((req, res, next) => {
-// User.findByPk(1)
-// .then(user => {
-//   req.user = user;
-//   next();
-// })
-// .catch(err => console.log(err));
-next();
+User.getById('5c3c6bae3dd8fd06f9d6ea3a')
+.then(user => {
+  req.user = new User(user.name, user.email, user.cart, user._id);
+  next();
+})
+.catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoute);
