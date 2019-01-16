5b63bd181d8b49e87d301b0b07ca4ea5198858d6
Ariel Nykanen <arielnykan3n@gmail.com>
2019-01-16 08:54:23 +0200

    adding sending email mechanism

--- course-total-progress/controllers/auth.js
+++ course-total-progress/controllers/auth.js
@@ -1,5 +1,13 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: ''
+  }
+}));
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -34,28 +42,37 @@ exports.postSignUp = (req, res, next) => {
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
   User.findOne({ email: email })
-  .then(userDoc => {
-    if (userDoc) {
-      req.flash('error', 'Email is already registred, please pick different one.');
-      return res.redirect('/signup');
-    }
-    return bcrypt.hash(password, 12)
-    .then(hashedPassword => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items: [] }
-      });
-      return user.save();
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash('error', 'Email is already registred, please pick different one.');
+        return res.redirect('/signup');
+      }
+      return bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@marker-supplies.com',
+            subject: 'Signup Succeeded!',
+            html: '<h1>You successfuly signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth sending email, at line 69 reason: ' + err + ' ');
+        })
+        
     })
-    .then(result => {
-      res.redirect('/login');
+    .catch(err => {
+      console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth CTRL, at line 24 reason: ' + err + ' ');
     });
-   
-  })
-  .catch(err => {
-    console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth CTRL, at line 24 reason: ' + err + ' ');
-  });
 }
 
 exports.postLogin = (req, res, next) => {
@@ -69,23 +86,23 @@ exports.postLogin = (req, res, next) => {
         return res.redirect('/login');
       }
       bcrypt.compare(password, user.password)
-      .then(doMatch => {
-        if (doMatch) {
-          req.session.isLoggedIn = true;
-          req.session.user = user;
-          return req.session.save(err => {
-            res.redirect('/');
-          });
-        }
-        req.flash('error', 'Invalid Email or Password.')
-        res.redirect('/login');
-      })
-      .catch(err => {
-        console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth password validation, at line 58 reason: ' + err + ' ');
-        res.redirect('/login');
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              res.redirect('/');
+            });
+          }
+          req.flash('error', 'Invalid Email or Password.')
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth password validation, at line 58 reason: ' + err + ' ');
+          res.redirect('/login');
 
-      })
-    
+        })
+
     })
     .catch(err => console.log(err));
 }
