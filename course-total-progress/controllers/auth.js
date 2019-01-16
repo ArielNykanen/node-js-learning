bdb8a4f9be42190c69c8e71c3fe26f9d833932ad
Ariel Nykanen <arielnykan3n@gmail.com>
2019-01-16 10:56:47 +0200

    adding reset password via email secured mechanism + views

--- course-total-progress/controllers/auth.js
+++ course-total-progress/controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const nodemailer = require('nodemailer');
@@ -23,6 +25,33 @@ exports.getLogin = (req, res, next) => {
   });
 }
 
+exports.newPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+      if(!user) {
+        return res.redirect('/login');
+      }
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token
+      });
+    })
+    .catch(err => {
+      console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth, at line 33 reason: ' + err + ' ');
+    });
+
+}
+
 exports.getSignUp = (req, res, next) => {
   let message = req.flash('error');
   if (message.length > 0) {
@@ -33,6 +62,19 @@ exports.getSignUp = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Sign-Up',
+    errorMessage: message
+  });
+}
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset-password', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
     errorMessage: message
   });
 }
@@ -68,7 +110,7 @@ exports.postSignUp = (req, res, next) => {
         .catch(err => {
           console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth sending email, at line 69 reason: ' + err + ' ');
         })
-        
+
     })
     .catch(err => {
       console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth CTRL, at line 24 reason: ' + err + ' ');
@@ -116,3 +158,70 @@ exports.postLogout = (req, res, next) => {
   });
 }
 
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth, at line 137 reason: ' + err + ' ');
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No Account With That Email Found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save()
+          .then(result => {
+            res.redirect('/');
+            transporter.sendMail({
+              to: req.body.email,
+              from: 'shop@marker-supplies.com',
+              subject: 'Password Reset',
+              html: `
+          <p>You requested a password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}" >Link </a> to set a new password</p>
+          `
+            });
+          });
+      })
+      .catch(err => {
+        console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth , at line 144 reason: ' + err + ' ');
+      });
+  });
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+  User.findOne({ resetToken: passwordToken, 
+    resetTokenExpiration: { $gt: Date.now() }, 
+    _id: userId 
+  })
+  .then(user => {
+    resetUser = user; 
+    console.log(resetUser);
+    
+    if (!user) {
+     return res.redirect('/login');
+    }
+    return bcrypt.hash(newPassword, 12)
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(result => {
+      res.redirect('/login'); 
+    });
+  })
+  
+  .catch(err => {
+    console.log('\x1b[31m%s\x1b[23m', 'Error!!!! from auth, at line 204 reason: ' + err + ' ');
+  })
+};
--- course-total-progress/routes/auth.js
+++ course-total-progress/routes/auth.js
@@ -6,9 +6,13 @@ const router = express.Router();
 
 router.get('/login', authCtrl.getLogin);
 router.get('/signup', authCtrl.getSignUp);
-
+router.get('/reset', authCtrl.getReset);
+router.get('/reset/:token', authCtrl.newPassword);
+ 
 router.post('/login', authCtrl.postLogin);
 router.post('/logout', authCtrl.postLogout);
 router.post('/signup', authCtrl.postSignUp);
+router.post('/reset', authCtrl.postReset);
+router.post('/new-password', authCtrl.postNewPassword);
 
 module.exports = router;
